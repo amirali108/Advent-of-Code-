c752aa002d42ba2d7ed55daaf34082401dd37264
amirali108 <117827939+amirali108@users.noreply.github.com>
2023-02-04 11:38:21 +0100

    Update day_01_part_02.py

--- day_01_part_02.py
+++ day_01_part_02.py
@@ -4,4 +4,21 @@ from day_01_part_01 import read_file
 
 
 def calculate_multi(fuels:list):
-    pass
+    sum=0
+    for item in fuels:
+        item=(item//3)-2
+        while item>0:
+            sum+=item
+            item=(item//3)-2
+    
+
+    return sum
+
+
+
+fuels=read_file("input_01")
+result=calculate_multi(fuels)
+print("answer part two:  ", result)
+
+
+
